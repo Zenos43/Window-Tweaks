a5cead5f8ee56c9abb27fb328a62801a0656dd08
Zenos43 <lazasa2002@gmail.com>
2025-06-25 13:10:15 +0700

    fixed the bbr2 issue with steam

--- Network Tweaks/Optional/BBRv2.bat
+++ Network Tweaks/Optional/BBRv2.bat
@@ -1,3 +1,4 @@
+netsh int ipv4 set gl loopbacklargemtu=disabled
 netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
